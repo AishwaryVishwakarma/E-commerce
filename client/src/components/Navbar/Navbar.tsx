2d9929527ad5aa42c81b77f4173ae2bf5a43f530
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-03-31 15:55:39 +0530

    added navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import { type Theme } from '../../model'
+import styles from './Navbar.module.scss'
 
 const Navbar: React.FC<Theme> = ({ theme, setTheme }) => {
-  return <div>Navbar</div>
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.navHeading}>
+        React<i className={`fab fa-react ${styles.navLogo}`}></i>
+      </h1>
+      <ul className={styles.navItems}>
+        <li>Home</li>
+        <li>Services</li>
+        <li>Products</li>
+        <li>Contact Us</li>
+      </ul>
+      <button type='button' className={styles.signupButton}>
+        Sign-Up
+      </button>
+    </nav>
+  )
 }
 
 export default Navbar
