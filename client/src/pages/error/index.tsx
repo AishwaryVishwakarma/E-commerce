3934f71fbfad1b0da26beea5c1fb7ce55b0db897
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-04-13 16:50:37 +0530

    added an error page

--- client/src/pages/error/index.tsx
+++ client/src/pages/error/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 import Layout from '../../components/Layout/Layout'
 import styles from './styles.module.scss'
 
 const ErrorPage: React.FC = () => {
+  const naviagte = useNavigate()
+  const [timer, setTimer] = useState(10000)
+
+  const decrementTimer = useCallback(() => {
+    setTimer((oldTimer) => oldTimer - 1)
+  }, [])
+
+  useEffect(() => {
+    if (timer <= 0) {
+      naviagte('/')
+      return
+    }
+    const timeoutFunction = setInterval(decrementTimer, 1000)
+
+    return () => clearInterval(timeoutFunction)
+  }, [decrementTimer, timer])
+
   return (
     <Layout>
-      <h1>ErrorPage</h1>
+      <div className={styles.errorWrapper}>
+        <img src='https://i.postimg.cc/bvmrDR9X/error-page.png'/>
+        <div className={styles.textContainer}>
+          <div className={styles.textHeading}>
+            Oh no! The page doesn't exist.
+          </div>
+          <div className={styles.subtext}>
+            Redirecting to <Link to='/'>homepage</Link> in {timer} seconds
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
