5c0e8df37265183c98c6a662455deda1d965ef7f
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-04-21 00:12:58 +0530

    added third fold

--- client/src/components/Home/Third-Fold/TFCard/TFCard.tsx
+++ client/src/components/Home/Third-Fold/TFCard/TFCard.tsx
@@ -1,12 +1,85 @@
 import React from 'react'
-import { type TFDummyData } from './../../../../model'
+import styles from './styles.module.scss'
+import { Stars, type TFDummyData } from './../../../../model'
+import { RiStarFill } from 'react-icons/ri'
 
 interface Data {
   data: TFDummyData
 }
 
+const STARS: Stars[] = [
+  {
+    element: <RiStarFill className={styles.starsIcon} />
+  },
+  {
+    element: <RiStarFill className={styles.starsIcon} />
+  },
+  {
+    element: <RiStarFill className={styles.starsIcon} />
+  },
+  {
+    element: <RiStarFill className={styles.starsIcon} />
+  },
+  {
+    element: <RiStarFill className={styles.starsIcon} />
+  }
+]
+
 const TFCard: React.FC<Data> = ({ data }) => {
-  return <div>TFCard</div>
+  const {
+    id,
+    title,
+    image,
+    discount,
+    discount_price,
+    original_price,
+    rating,
+    delivery_date,
+    free_delivery,
+    category
+  } = data ?? {}
+
+  return (
+    <div className={styles.cardWrapper}>
+      <img className={styles.prodImage} src={image} alt={title} />
+      <div className={styles.productName}>{title}</div>
+      <div className={styles.rating}>
+        {STARS.map((star, idx) => star.element)}
+      </div>
+      <>
+        {discount ? (
+          <div className={styles.blockBusterDeal}>
+            <p>{discount}% off</p> <span>BlockBuster of the day</span>
+          </div>
+        ) : (
+          <div className={styles.bestSellerDeal}>
+            <img
+              className={styles.bestSellerImage}
+              src='https://i.postimg.cc/ZRhpvcTZ/best-seller.png'
+            />
+            <span>in {category}</span>
+          </div>
+        )}
+      </>
+      <div className={styles.discountPrice}>
+        {discount ? `₹${discount_price}.00` : `₹${original_price}.00`}
+      </div>
+      <div className={styles.originalPrice}>
+        {discount ? (
+          <p>
+            M.R.P: <s>₹{original_price}.00</s>
+          </p>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className={styles.freeDelivery}>
+        <img src='https://i.postimg.cc/W3BTsZ3C/amazon-prime-icon-1.png' />{' '}
+        <p>FREE Delivery</p>
+      </div>
+      <div className={styles.deliveryDate}>{delivery_date}</div>
+    </div>
+  )
 }
 
 export default TFCard
