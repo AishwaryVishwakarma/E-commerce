4e3dfa02bc31f5e0119615e3110f18b3b9ef450b
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2023-04-22 17:07:10 +0530

    removed generic card

--- client/src/components/Commons/Card/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Card } from '../../../model'
-import TodaysDeal from "../../Home/First-Fold/FirstFold"
-
-const Card: React.FC<Card> = ({ type, data }) => {
-  return <div>{type === 'todays-deal' && <TodaysDeal />}</div>
-}
-
-export default Card
